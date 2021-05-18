c021cfb21833063013321381100f7cfdd6394a88
Aiji <msouldze1@gmail.com>
2021-05-18 18:30:57 +0900

    add: new actions and new function

---
+++ src/commander.js
@@ -0,0 +1,15 @@
+import { Command } from 'commander';
+import genDiff from './index.js';
+
+const program = new Command();
+
+program
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
+
+export default program;
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+import { readFileSync } from 'fs';
+
+const getFileData = (fileName) => {
+  const data = readFileSync(fileName, 'utf8');
+  return JSON.parse(data);
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const fileData1 = getFileData(filepath1);
+  const fileData2 = getFileData(filepath2);
+  const f1 = Object.entries(fileData1).map((str) => str.join(': '));
+  const f2 = Object.entries(fileData2).map((str) => str.join(': '));
+  const unitedData = _.union(f1, f2)
+    .sort()
+    .reduce((result, data) => {
+      const key = data.slice(0, data.indexOf(':'));
+      const value = data.slice(data.indexOf(':'));
+      if (_.has(fileData1, key) && _.has(fileData2, key)) {
+        if (fileData1[key] !== value) {
+          result.push(`+ ${data}`);
+        } else if (fileData2[data] !== value) {
+          result.push(`- ${data}`);
+        } else {
+          result.push(`  ${data}`);
+        }
+      }
+      if (!_.has(fileData1, key) && _.has(fileData2, key)) {
+        result.push(`+ ${data}`);
+      }
+      if (_.has(fileData1, key) && !_.has(fileData2, key)) {
+        result.push(`- ${data}`);
+      }
+      return result;
+    }, [])
+    .join('\n');
+  console.log(_.union(f1, f2).sort());
+  return `{\n${unitedData}\n}`;
+};
+
+export default genDiff;
