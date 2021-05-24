52b46533aaa4c243427c04ac800d333824c1bcf2
Aiji <msouldze1@gmail.com>
2021-05-24 20:48:05 +0900

    fix: pass tests

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ const genDiff = (filepath1, filepath2) => {
   const dataEntries1 = Object.entries(fileData1);
   const dataEntries2 = Object.entries(fileData2);
   const unitedData = _.sortBy(_.union(dataEntries1, dataEntries2))
+    .sort((a, b) => {
+      if (a[0] === b[0]) {
+        return b[1] - a[1];
+      }
+      return b[0] - a[0];
+    })
     .reduce((result, [key, value]) => {
       if (_.has(fileData1, key) && _.has(fileData2, key)) {
         if (fileData1[key] !== value) {
