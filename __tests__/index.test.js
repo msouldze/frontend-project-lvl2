37cb24823b0db727b06d080e0552a5dd9ac3c785
Aiji <msouldze1@gmail.com>
2021-05-26 22:40:36 +0900

    add: new test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,15 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('difference generator', () => {
+test('json format comparison', () => {
   const actual = genDiff('file1.json', 'file2.json');
+  const expected = readFile('result.txt');
+
+  expect(actual).toEqual(expected);
+});
+
+test('yaml format comparison', () => {
+  const actual = genDiff('filepath1.yml', 'filepath2.yaml');
   const expected = readFile('result.txt');
 
   expect(actual).toEqual(expected);
