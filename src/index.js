f122506b70cf90c07ece8560f97038b15b3dc3a1
Aiji <msouldze1@gmail.com>
2021-05-26 22:45:10 +0900

    refactor: gendiff function

--- src/index.js
+++ src/index.js
@@ -1,43 +1,51 @@
 import _ from 'lodash';
 import path from 'path';
 import { readFileSync } from 'fs';
+import parseFile from './parsers.js';
 
-const readFileData = (filePath) => {
-  const data = readFileSync(path.resolve(process.cwd(), '__fixtures__', filePath), 'utf8');
-  return JSON.parse(data);
-};
+const getFilePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
+const getFileData = (filename) => readFileSync(getFilePath(filename), 'utf8');
 
-const genDiff = (filepath1, filepath2) => {
-  const fileData1 = readFileData(filepath1);
-  const fileData2 = readFileData(filepath2);
+const genDiff = (filename1, filename2) => {
+  const fileData1 = parseFile(filename1, getFileData(filename1));
+  const fileData2 = parseFile(filename2, getFileData(filename2));
   const dataEntries1 = Object.entries(fileData1);
   const dataEntries2 = Object.entries(fileData2);
   const unitedData = _.sortBy(_.union(dataEntries1, dataEntries2))
-    .sort((a, b) => {
-      if (a[0] === b[0]) {
-        return b[1] - a[1];
-      }
-      return b[0] - a[0];
-    })
     .reduce((result, [key, value]) => {
+      const newKey = `${key}:`;
       if (_.has(fileData1, key) && _.has(fileData2, key)) {
         if (fileData1[key] !== value) {
-          result.push(`  + ${key}: ${value}`);
+          result.push([' ', '+', newKey, value]);
         } else if (fileData2[key] !== value) {
-          result.push(`  - ${key}: ${value}`);
+          result.push([' ', '-', newKey, value]);
         } else {
-          result.push(`    ${key}: ${value}`);
+          result.push([' ', ' ', newKey, value]);
         }
       }
       if (!_.has(fileData1, key) && _.has(fileData2, key)) {
-        result.push(`  + ${key}: ${value}`);
+        result.push([' ', '+', newKey, value]);
       }
       if (_.has(fileData1, key) && !_.has(fileData2, key)) {
-        result.push(`  - ${key}: ${value}`);
+        result.push([' ', '-', newKey, value]);
       }
       return result;
     }, []);
-  const result = _.uniq(unitedData).join('\n');
+  const sortedData = unitedData
+    .sort((a, b) => {
+      if (a[2] === b[2]) {
+        if (a[1] < b[1]) {
+          return 1;
+        }
+        if (a[1] > b[1]) {
+          return -1;
+        }
+        return 0;
+      }
+      return 1;
+    })
+    .map((data) => data.join(' '));
+  const result = _.uniq(sortedData).join('\n');
   return `{\n${result}\n}`;
 };
 
