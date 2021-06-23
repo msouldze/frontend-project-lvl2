a543985ab15a1d8be626316570a7bd6bed97abc4
Aiji <msouldze1@gmail.com>
2021-06-23 23:04:43 +0900

    fix: tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import genDiff from '../src/index.js';
+import { genDiff, stylish } from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,14 +10,14 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('json format comparison', () => {
-  const actual = genDiff('file1.json', 'file2.json');
+  const actual = stylish(genDiff('file1.json', 'file2.json'));
   const expected = readFile('result.txt');
 
   expect(actual).toEqual(expected);
 });
 
 test('yaml format comparison', () => {
-  const actual = genDiff('filepath1.yml', 'filepath2.yaml');
+  const actual = stylish(genDiff('file1.json', 'file2.json'));
   const expected = readFile('result.txt');
 
   expect(actual).toEqual(expected);
