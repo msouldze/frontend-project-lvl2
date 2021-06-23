de5f036fd0e934b4e6662dd2a15c9c44ffa3d97e
Aiji <msouldze1@gmail.com>
2021-06-23 23:01:58 +0900

    change function

--- src/commander.js
+++ src/commander.js
@@ -1,6 +1,5 @@
-import path from 'path';
 import { Command } from 'commander';
-import genDiff from './index.js';
+import { genDiff, stylish } from './index.js';
 
 const program = new Command();
 
@@ -8,14 +7,12 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', stylish)
   .action((filepath1, filepath2, options) => {
     if (options.format) {
-      const format1 = path.extname(filepath1);
-      const format2 = path.extname(filepath2);
-      console.log(format1, format2);
+      console.log(genDiff(filepath1, filepath2));
     }
-    console.log(genDiff(filepath1, filepath2));
+    console.log(stylish(genDiff(filepath1, filepath2)));
   });
 
 export default program;
--- src/index.js
+++ src/index.js
@@ -5,48 +5,69 @@ import parseFile from './parsers.js';
 
 const getFilePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const getFileData = (filename) => readFileSync(getFilePath(filename), 'utf8');
+const isObject = (data) => data != null && typeof data === 'object';
 
-const genDiff = (filename1, filename2) => {
+export const genDiff = (filename1, filename2) => {
   const fileData1 = parseFile(filename1, getFileData(filename1));
   const fileData2 = parseFile(filename2, getFileData(filename2));
-  const dataEntries1 = Object.entries(fileData1);
-  const dataEntries2 = Object.entries(fileData2);
-  const unitedData = _.sortBy(_.union(dataEntries1, dataEntries2))
-    .reduce((result, [key, value]) => {
-      const newKey = `${key}:`;
-      if (_.has(fileData1, key) && _.has(fileData2, key)) {
-        if (fileData1[key] !== value) {
-          result.push([' ', '+', newKey, value]);
-        } else if (fileData2[key] !== value) {
-          result.push([' ', '-', newKey, value]);
-        } else {
-          result.push([' ', ' ', newKey, value]);
+  const iter = (data1, data2) => {
+    if (!isObject(data1) || !isObject(data2)) {
+      return data1 !== undefined ? String(data1) : String(data2);
+    }
+    if (!isObject(data1) && !isObject(data2)) {
+      return !isObject(data1) ? String(data1) : String(data2);
+    }
+
+    const dataEntries1 = Object.entries(data1);
+    const dataEntries2 = Object.entries(data2);
+    const unitedData = _.sortBy(_.unionWith(dataEntries1, dataEntries2, _.isEqual))
+      .map(([key, value]) => {
+        if (_.has(data1, key) && _.has(data2, key)) {
+          if ((isObject(data1[key]) && isObject(data2[key])) || data1[key] === data2[key]) {
+            return [' ', `${key}:`, iter(data1[key], data2[key])];
+          }
+          if (data1[key] !== data2[key]) {
+            if (data1[key] === value) {
+              return ['-', `${key}:`, iter(data1[key], data1[key])];
+            }
+            return ['+', `${key}:`, iter(data2[key], data2[key])];
+          }
         }
-      }
-      if (!_.has(fileData1, key) && _.has(fileData2, key)) {
-        result.push([' ', '+', newKey, value]);
-      }
-      if (_.has(fileData1, key) && !_.has(fileData2, key)) {
-        result.push([' ', '-', newKey, value]);
-      }
-      return result;
-    }, []);
-  const sortedData = unitedData
-    .sort((a, b) => {
-      if (a[2] === b[2]) {
-        if (a[1] < b[1]) {
-          return 1;
+        return (!_.has(data1, key) && _.has(data2, key)) ? ['+', `${key}:`, iter(data2[key], data2[key])] : ['-', `${key}:`, iter(data1[key], data1[key])];
+      });
+    const result = _.uniqWith(unitedData, _.isEqual)
+      .sort((a, b) => {
+        if (a[1] === b[1]) {
+          if (a[0] < b[0]) {
+            return 1;
+          }
+          if (a[0] > b[0]) {
+            return -1;
+          }
+          return 0;
         }
-        if (a[1] > b[1]) {
-          return -1;
-        }
-        return 0;
-      }
-      return 1;
-    })
-    .map((data) => data.join(' '));
-  const result = _.uniq(sortedData).join('\n');
-  return `{\n${result}\n}`;
+        return 1;
+      });
+    return result;
+  };
+  return iter(fileData1, fileData2);
 };
 
-export default genDiff;
+export const stylish = (data, replacer = ' ', spacesCount = 2) => {
+  const iter = (tree, depth) => {
+    if (!Array.isArray(tree)) {
+      return tree;
+    }
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const lines = tree
+      .map(([diff, key, val]) => `${currentIndent}${diff} ${key} ${iter(val, depth + 2)}`);
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+  return iter(data, 1);
+};
