7e87a1828585b240f6fe6c04615d4adf337ce3bd
Aiji <msouldze1@gmail.com>
2021-06-27 23:16:28 +0900

    fix: add styles in gendiff

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import _ from 'lodash';
 import path from 'path';
 import { readFileSync } from 'fs';
 import parseFile from './parsers.js';
+import stylish from '../formatters/stylish.js';
 
 const getFilePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const getFileData = (filename) => readFileSync(getFilePath(filename), 'utf8');
 const isObject = (data) => data != null && typeof data === 'object';
 
-export const genDiff = (filename1, filename2) => {
+const genDiff = (filename1, filename2, formatName = stylish) => {
   const fileData1 = parseFile(filename1, getFileData(filename1));
   const fileData2 = parseFile(filename2, getFileData(filename2));
   const iter = (data1, data2) => {
@@ -24,16 +25,16 @@ export const genDiff = (filename1, filename2) => {
       .map(([key, value]) => {
         if (_.has(data1, key) && _.has(data2, key)) {
           if ((isObject(data1[key]) && isObject(data2[key])) || data1[key] === data2[key]) {
-            return [' ', `${key}:`, iter(data1[key], data2[key])];
+            return [' ', `${key}`, iter(data1[key], data2[key])];
           }
           if (data1[key] !== data2[key]) {
             if (data1[key] === value) {
-              return ['-', `${key}:`, iter(data1[key], data1[key])];
+              return ['-', `${key}`, iter(data1[key], data1[key])];
             }
-            return ['+', `${key}:`, iter(data2[key], data2[key])];
+            return ['+', `${key}`, iter(data2[key], data2[key])];
           }
         }
-        return (!_.has(data1, key) && _.has(data2, key)) ? ['+', `${key}:`, iter(data2[key], data2[key])] : ['-', `${key}:`, iter(data1[key], data1[key])];
+        return (!_.has(data1, key) && _.has(data2, key)) ? ['+', `${key}`, iter(data2[key], data2[key])] : ['-', `${key}`, iter(data1[key], data1[key])];
       });
     const result = _.uniqWith(unitedData, _.isEqual)
       .sort((a, b) => {
@@ -50,24 +51,7 @@ export const genDiff = (filename1, filename2) => {
       });
     return result;
   };
-  return iter(fileData1, fileData2);
+  return formatName(iter(fileData1, fileData2));
 };
 
-export const stylish = (data, replacer = ' ', spacesCount = 2) => {
-  const iter = (tree, depth) => {
-    if (!Array.isArray(tree)) {
-      return tree;
-    }
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const lines = tree
-      .map(([diff, key, val]) => `${currentIndent}${diff} ${key} ${iter(val, depth + 2)}`);
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-  return iter(data, 1);
-};
+export default genDiff;
