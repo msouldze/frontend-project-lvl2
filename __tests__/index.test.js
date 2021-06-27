0bddbef90ff21e7319b5baac572cd4b665cd3b53
Aiji <msouldze1@gmail.com>
2021-06-27 23:13:33 +0900

    add: new test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,23 +2,28 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import { genDiff, stylish } from '../src/index.js';
+import genDiff from '../src/index.js';
+import plain from '../formatters/plain.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('json format comparison', () => {
-  const actual = stylish(genDiff('file1.json', 'file2.json'));
+test('stylish', () => {
+  const actualJson = genDiff('file1.json', 'file2.json');
+  const actualYaml = genDiff('filepath1.yml', 'filepath2.yaml');
   const expected = readFile('result.txt');
 
-  expect(actual).toEqual(expected);
+  expect(actualJson).toEqual(expected);
+  expect(actualYaml).toEqual(expected);
 });
 
-test('yaml format comparison', () => {
-  const actual = stylish(genDiff('file1.json', 'file2.json'));
-  const expected = readFile('result.txt');
+test('plain', () => {
+  const actualJson = genDiff('file1.json', 'file2.json', plain);
+  const actualYaml = genDiff('filepath1.yml', 'filepath2.yaml', plain);
+  const expected = readFile('resultPlain.txt');
 
-  expect(actual).toEqual(expected);
+  expect(actualJson).toEqual(expected);
+  expect(actualYaml).toEqual(expected);
 });
