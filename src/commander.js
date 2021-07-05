6a3d8521cf8314abc082700ec3894a2ec0a11238
Aiji <msouldze1@gmail.com>
2021-07-05 23:03:38 +0900

    change

--- src/commander.js
+++ src/commander.js
@@ -13,7 +13,6 @@ program
   .option('-f, --format [type]', 'output format', stylish)
   .action((filepath1, filepath2, options) => {
     const fileDiff = genDiff(filepath1, filepath2);
-    const stylishDiff = genDiff(filepath1, filepath2, stylish);
     const plainDiff = genDiff(filepath1, filepath2, plain);
     const jsonDiff = genDiff(filepath1, filepath2, json);
     if (options.format === 'plain') {
