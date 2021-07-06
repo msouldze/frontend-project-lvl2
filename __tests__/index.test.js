2cb1f949fe5ea69ef41bae62fd576c0c41fac1c3
Aiji <msouldze1@gmail.com>
2021-07-06 23:12:29 +0900

    fix: tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import genDiff from '../src/formatters/index.js';
+import formatter from '../src/formatters/formatter.js';
 import plain from '../src/formatters/plain.js';
 import json from '../src/formatters/json.js';
 
@@ -12,17 +12,17 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
-  { actualJson: genDiff('file1.json', 'file2.json'), expected: readFile('resultStylish.txt') },
-  { actualJson: genDiff('file1.json', 'file2.json', plain), expected: readFile('resultPlain.txt') },
-  { actualJson: genDiff('file1.json', 'file2.json', json), expected: readFile('resultJson.txt') },
+  { actualJson: formatter('file1.json', 'file2.json'), expected: readFile('resultStylish.txt') },
+  { actualJson: formatter('file1.json', 'file2.json', plain), expected: readFile('resultPlain.txt') },
+  { actualJson: formatter('file1.json', 'file2.json', json), expected: readFile('resultJson.txt') },
 ])('json', ({ actualJson, expected }) => {
   expect(actualJson).toBe(expected);
 });
 
 test.each([
-  { actualYaml: genDiff('file1.yml', 'file2.yaml'), expected: readFile('resultStylish.txt') },
-  { actualYaml: genDiff('file1.yml', 'file2.yaml', plain), expected: readFile('resultPlain.txt') },
-  { actualYaml: genDiff('file1.yml', 'file2.yaml', json), expected: readFile('resultJson.txt') },
+  { actualYaml: formatter('file1.yml', 'file2.yaml'), expected: readFile('resultStylish.txt') },
+  { actualYaml: formatter('file1.yml', 'file2.yaml', plain), expected: readFile('resultPlain.txt') },
+  { actualYaml: formatter('file1.yml', 'file2.yaml', json), expected: readFile('resultJson.txt') },
 ])('yaml', ({ actualYaml, expected }) => {
   expect(actualYaml).toBe(expected);
 });
