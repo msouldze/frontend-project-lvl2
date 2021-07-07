6e4aef401d4fd3386c3439f4139b494c6b12992c
Aiji <msouldze1@gmail.com>
2021-07-07 22:34:29 +0900

    delete: file

--- src/formatters/formatter.js
+++
@@ -1,9 +0,0 @@
-import genDiff from './index.js';
-import stylish from './stylish.js';
-
-const formatter = (data1, data2, formatName = stylish) => {
-  const diff = genDiff(data1, data2);
-  return formatName(diff);
-};
-
-export default formatter;
