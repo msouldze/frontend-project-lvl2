d056bd5521f13a31e918f197a37abfeb327ad68a
Aiji <msouldze1@gmail.com>
2021-07-10 22:37:40 +0900

    fix: tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,9 +3,6 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 import formatter from '../src/formatters/index.js';
-import stylish from '../src/formatters/stylish.js';
-import plain from '../src/formatters/plain.js';
-import json from '../src/formatters/json.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,18 +11,18 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 test.each([
   { actualJson: formatter('file1.json', 'file2.json'), expected: readFile('resultStylish.txt') },
-  { actualJson: formatter('file1.json', 'file2.json', stylish), expected: readFile('resultStylish.txt') },
-  { actualJson: formatter('file1.json', 'file2.json', plain), expected: readFile('resultPlain.txt') },
-  { actualJson: formatter('file1.json', 'file2.json', json), expected: readFile('resultJson.txt') },
+  { actualJson: formatter('file1.json', 'file2.json', 'stylish'), expected: readFile('resultStylish.txt') },
+  { actualJson: formatter('file1.json', 'file2.json', 'plain'), expected: readFile('resultPlain.txt') },
+  { actualJson: formatter('file1.json', 'file2.json', 'json'), expected: readFile('resultJson.txt') },
 ])('json', ({ actualJson, expected }) => {
   expect(actualJson).toBe(expected);
 });
 
 test.each([
   { actualYaml: formatter('file1.yml', 'file2.yaml'), expected: readFile('resultStylish.txt') },
-  { actualJson: formatter('file1.yml', 'file2.yaml', stylish), expected: readFile('resultStylish.txt') },
-  { actualYaml: formatter('file1.yml', 'file2.yaml', plain), expected: readFile('resultPlain.txt') },
-  { actualYaml: formatter('file1.yml', 'file2.yaml', json), expected: readFile('resultJson.txt') },
+  { actualJson: formatter('file1.yml', 'file2.yaml', 'stylish'), expected: readFile('resultStylish.txt') },
+  { actualYaml: formatter('file1.yml', 'file2.yaml', 'plain'), expected: readFile('resultPlain.txt') },
+  { actualYaml: formatter('file1.yml', 'file2.yaml', 'json'), expected: readFile('resultJson.txt') },
 ])('yaml', ({ actualYaml, expected }) => {
   expect(actualYaml).toBe(expected);
 });
