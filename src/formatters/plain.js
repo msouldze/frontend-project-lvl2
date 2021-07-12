e4c08861289776d65f7ae68e56ea2b66603cefdf
Aiji <msouldze1@gmail.com>
2021-07-12 20:05:30 +0900

    fix: format

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,21 +8,26 @@ const isObject = (value) => {
 };
 
 const plain = (data) => {
-  const iter = (tree, depth = '') => { 
+  const iter = (tree, depth = '') => {
+    if (!Array.isArray(tree)) {
+      return tree;
+    }
     const lines = tree
       .flatMap(([diff, key, val], index, array) => {
+        console.log(array);
         if (diff === '+') {
           return `Property '${depth}${key}' was added with value: ${isObject(val)}`;
         }
         if (diff === '-') {
           const [, key2, val2] = array[index + 1];
           if (key === key2) {
-            array[index + 1].splice(0, array.length);
+            array[index + 1].pop();
+            array[index + 1].pop();
+            array[index + 1].pop();
+            // array[index + 1].splice(0, array.length);
             return `Property '${depth}${key}' was updated. From ${isObject(val)} to ${isObject(val2)}`;
           }
           return `Property '${depth}${key}' was removed`;
-        } if (diff === undefined) {
-          return [];
         }
         return (typeof val === 'object') ? iter(val, `${depth}${key}.`) : [];
       });
