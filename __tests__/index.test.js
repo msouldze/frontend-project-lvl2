5c6e6790234af883f68302201f5b3c7d958163e3
Aiji <msouldze1@gmail.com>
2021-07-21 22:22:49 +0900

    fix: tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import formatter from '../src/formatters/index.js';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,19 +10,11 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
-  { actualJson: formatter('file1.json', 'file2.json'), expected: readFile('resultStylish.txt') },
-  { actualJson: formatter('file1.json', 'file2.json', 'stylish'), expected: readFile('resultStylish.txt') },
-  { actualJson: formatter('file1.json', 'file2.json', 'plain'), expected: readFile('resultPlain.txt') },
-  { actualJson: formatter('file1.json', 'file2.json', 'json'), expected: readFile('resultJson.txt') },
-])('json', ({ actualJson, expected }) => {
+  { actualJson: genDiff('file1.json', 'file2.json'), actualYaml: genDiff('file1.yml', 'file2.yaml'), expected: readFile('resultStylish.txt') },
+  { actualJson: genDiff('file1.json', 'file2.json', 'stylish'), actualYaml: genDiff('file1.yml', 'file2.yaml', 'stylish'), expected: readFile('resultStylish.txt') },
+  { actualJson: genDiff('file1.json', 'file2.json', 'plain'), actualYaml: genDiff('file1.yml', 'file2.yaml', 'plain'), expected: readFile('resultPlain.txt') },
+  { actualJson: genDiff('file1.json', 'file2.json', 'json'), actualYaml: genDiff('file1.yml', 'file2.yaml', 'json'), expected: readFile('resultJson.txt') },
+])('gendiff', ({ actualJson, actualYaml, expected }) => {
   expect(actualJson).toBe(expected);
-});
-
-test.each([
-  { actualYaml: formatter('file1.yml', 'file2.yaml'), expected: readFile('resultStylish.txt') },
-  { actualYaml: formatter('file1.yml', 'file2.yaml', 'stylish'), expected: readFile('resultStylish.txt') },
-  { actualYaml: formatter('file1.yml', 'file2.yaml', 'plain'), expected: readFile('resultPlain.txt') },
-  { actualYaml: formatter('file1.yml', 'file2.yaml', 'json'), expected: readFile('resultJson.txt') },
-])('yaml', ({ actualYaml, expected }) => {
   expect(actualYaml).toBe(expected);
 });
