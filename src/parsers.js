91a23d939fd4686d4ffa75aff8e782697865cbe8
Aiji <msouldze1@gmail.com>
2021-07-21 22:25:26 +0900

    fix: parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,11 +1,11 @@
 import yaml from 'js-yaml';
 import path from 'path';
 
-const parseFile = (filename, data) => {
+const parseFile = (filename) => {
   const format = path.extname(filename);
 
   // Выбирается функция-парсер в зависимости от расширения файла
-  const parse = (format === '.yml' || format === '.yaml') ? yaml.load(data) : JSON.parse(data);
+  const parse = (format === '.yml' || format === '.yaml') ? yaml.load : JSON.parse;
   return parse;
 };
 
