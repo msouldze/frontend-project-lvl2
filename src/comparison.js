f7707400ccde5e8bfa7d68b58186784d939248bc
Aiji <msouldze1@gmail.com>
2021-07-21 22:25:58 +0900

    add: new module

---
+++ src/comparison.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+const getDiff = (data1, data2) => {
+  if (!_.isObject(data1) || !_.isObject(data2)) {
+    return data1 !== undefined ? String(data1) : String(data2);
+  }
+  if (!_.isObject(data1) && !_.isObject(data2)) {
+    return !_.isObject(data1) ? String(data1) : String(data2);
+  }
+
+  const dataEntries1 = Object.entries(data1);
+  const dataEntries2 = Object.entries(data2);
+  const unitedData = _.unionWith(dataEntries1, dataEntries2, _.isEqual)
+    .map(([key, value]) => {
+      if (_.has(data1, key) && _.has(data2, key)) {
+        if ((_.isObject(data1[key]) && _.isObject(data2[key])) || data1[key] === data2[key]) {
+          return [' ', `${key}`, getDiff(data1[key], data2[key])];
+        }
+        if (data1[key] !== data2[key]) {
+          if (data1[key] === value) {
+            return ['-', `${key}`, getDiff(data1[key], data1[key])];
+          }
+          return ['+', `${key}`, getDiff(data2[key], data2[key])];
+        }
+      }
+      return (!_.has(data1, key) && _.has(data2, key)) ? ['+', `${key}`, getDiff(data2[key], data2[key])] : ['-', `${key}`, getDiff(data1[key], data1[key])];
+    });
+  const result = _.uniqWith(unitedData, _.isEqual);
+  const sorted = _.sortBy(result, (item) => {
+    const [symbol, key] = item;
+    return [key, -symbol];
+  });
+  return sorted;
+};
+
+export default getDiff;
