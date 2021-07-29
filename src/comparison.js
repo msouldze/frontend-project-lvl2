f95a574ef7ed76cc3067007c7246903eec0fe515
Aiji <msouldze1@gmail.com>
2021-07-29 21:39:21 +0900

    change: main flow

--- src/comparison.js
+++ src/comparison.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const getDiff = (data1, data2) => {
+const buildTree = (data1, data2) => {
   if (!_.isObject(data1) || !_.isObject(data2)) {
     return data1 !== undefined ? String(data1) : String(data2);
   }
@@ -10,20 +10,20 @@ const getDiff = (data1, data2) => {
 
   const dataEntries1 = Object.entries(data1);
   const dataEntries2 = Object.entries(data2);
-  const unitedData = _.unionWith(dataEntries1, dataEntries2, _.isEqual)
+  const unitedData = _.union(dataEntries1, dataEntries2)
     .map(([key, value]) => {
       if (_.has(data1, key) && _.has(data2, key)) {
         if ((_.isObject(data1[key]) && _.isObject(data2[key])) || data1[key] === data2[key]) {
-          return [' ', `${key}`, getDiff(data1[key], data2[key])];
+          return [' ', `${key}`, buildTree(data1[key], data2[key])];
         }
         if (data1[key] !== data2[key]) {
           if (data1[key] === value) {
-            return ['-', `${key}`, getDiff(data1[key], data1[key])];
+            return ['-', `${key}`, buildTree(data1[key], data1[key])];
           }
-          return ['+', `${key}`, getDiff(data2[key], data2[key])];
+          return ['+', `${key}`, buildTree(data2[key], data2[key])];
         }
       }
-      return (!_.has(data1, key) && _.has(data2, key)) ? ['+', `${key}`, getDiff(data2[key], data2[key])] : ['-', `${key}`, getDiff(data1[key], data1[key])];
+      return (!_.has(data1, key) && _.has(data2, key)) ? ['+', `${key}`, buildTree(data2[key], data2[key])] : ['-', `${key}`, buildTree(data1[key], data1[key])];
     });
   const result = _.uniqWith(unitedData, _.isEqual);
   const sorted = _.sortBy(result, (item) => {
@@ -33,4 +33,4 @@ const getDiff = (data1, data2) => {
   return sorted;
 };
 
-export default getDiff;
+export default buildTree;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import diff from './diff.js';
-import stylish from './formatters/stylish.js';
-import plain from './formatters/plain.js';
-import json from './formatters/json.js';
+import path from 'path';
+import { readFileSync } from 'fs';
+import parseFile from './parsers.js';
+import buildTree from './comparison.js';
+import getFormatter from './formatters/formatter.js';
 
-const genDiff = (data1, data2, formatName = 'stylish') => {
-  const formats = {
-    stylish,
-    plain,
-    json,
-  };
-  const diffResult = diff(data1, data2);
-  return formats[formatName](diffResult);
+const getFilePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFilePath(filename), 'utf8');
+const getFileFormat = (filename) => path.extname(filename);
+
+const genDiff = (filename1, filename2, formatName = 'stylish') => {
+  // Read and parse the first file's data
+  const data1 = readFile(filename1);
+  const dataFormat1 = getFileFormat(filename1);
+  const parsedData1 = parseFile(data1, dataFormat1);
+
+  // Read and parse the second file's data
+  const data2 = readFile(filename2);
+  const dataFormat2 = getFileFormat(filename2);
+  const parsedData2 = parseFile(data2, dataFormat2);
+
+  // Compare two files' data and format their results
+  const diffResult = buildTree(parsedData1, parsedData2);
+  const formatDiff = getFormatter(formatName);
+  return formatDiff(diffResult);
 };
 
 export default genDiff;
